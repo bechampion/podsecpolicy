cf690e97bc9d447b2c1c33023e97b69c2741deeb
jgarcia <jeronimo.garcia@asos.com>
2019-03-06 14:43:00 +0000

    Fully functional now

--- create.sh
+++ create.sh
@@ -1,1 +1,10 @@
 gcloud beta container clusters create standard-cluster-1 --enable-pod-security-policy --region us-central1-a
+gcloud container clusters get-credentials standard-cluster-1 --region us-central1-a
+kubectl get pod --all-namespaces
+kubectl create namespace example
+kubectl apply -f all.yaml -n example
+echo "This should fail"
+kubectl --as=system:serviceaccount:example:jeronimo -n example apply -f privpod.yaml
+echo "This should work"
+kubectl -n example apply -f privpod.yaml
+
